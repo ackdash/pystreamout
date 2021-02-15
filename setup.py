2fafd1644b42a7a0cbff9228cc682c855eaaae39
Rob <30823142+ackdash@users.noreply.github.com>
2021-02-16 10:16:37 +1100

    Bump required Python ver to 3.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Rob Boyle",
     author_email='rdb.code@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
