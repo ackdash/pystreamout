c0cfb472d7c288fba7fcb0fb01eb7456f2024dc9
Rob <30823142+ackdash@users.noreply.github.com>
2021-02-16 09:07:46 +1100

    Refactor

--- pystreamout/__init__.py
+++ pystreamout/__init__.py
@@ -1,8 +1,8 @@
 """Top-level package for pystreamout."""
 
 __author__ = """Rob Boyle"""
-__email__ = 'rdb.code@gmail.com'
-__version__ = '0.0.1'
+__email__ = "rdb.code@gmail.com"
+__version__ = "0.0.1"
 
 from .pystreamout import Streamer
 
